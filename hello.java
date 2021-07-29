2961b70e8a6bb335e36a7a5d2a03e2e5150fdbfa
MRazaRizvi <razarizvi447@gmail.com>
2021-07-29 11:13:34 +0500

    added index.html | modified hello.java

--- hello.java
+++ hello.java
@@ -5,7 +5,7 @@ public class hello{
          for(int i=1;i<=10;i++){
                   System.out.println("Hello world...."+i);
 
-      System.out.println("Hi");
+      System.out.println("Hello");
 
 }
 
